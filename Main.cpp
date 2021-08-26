3c7c22c7a5f7ba8e0ed781574758e9bdd4839332
Rayven <rayven.burns@gmail.com>
2021-08-26 11:15:25 -0400

    added window hints to specify version and package functions

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,13 @@
 
 int main()
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //specify the version of glfw thats being used
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //package of functions
+
+	
+	glfwTerminate();
 	return 0;
 }
